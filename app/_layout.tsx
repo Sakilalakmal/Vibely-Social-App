0844482f23e181b8277edfbddd365f75cfe9f98c
sakilalakmal <sakilalakmal77@gmail.com>
2025-11-05 13:06:39 +0530

    add comment features and create new functionality for add comment and auto updated comment count on posts and create notification when user commented and sent all data to post owner

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,13 +2,31 @@ import { tokenCache } from "@/cache";
 import InitialLayout from "@/components/InitialLayout";
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 import { ClerkLoaded, ClerkProvider } from "@clerk/clerk-expo";
+import { SplashScreen } from "expo-router";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { useFonts } from "expo-font";
+import { useCallback } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "JetBrainsMono-Medium": require("../assets/fonts/JetBrainsMono-Medium.ttf"),
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
   return (
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "black" }}>
+        <SafeAreaView
+          style={{ flex: 1, backgroundColor: "black" }}
+          onLayout={onLayoutRootView}
+        >
           <InitialLayout />
         </SafeAreaView>
       </SafeAreaProvider>
